6e61d5b130b64999ad0e8f542244334b6d89e993
Almir <almir.tihak98@hotmail.com>
2020-09-18 03:17:55 +0200

    CRUD operation done

---
+++ JobSearch.WebAPI/Controllers/BaseCRUDController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobSearch.WebAPI.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSearch.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseCRUDController<TModel, Tsearch, TInsert, TUpdate> : BaseController<TModel,Tsearch>
+    {
+        protected readonly ICRUDService<TModel,Tsearch,TInsert,TUpdate> _service=null;
+        public BaseCRUDController(ICRUDService<TModel, Tsearch, TInsert, TUpdate> service): base(service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public TModel Insert(TInsert request)
+        {
+            return _service.Insert(request);
+        }
+
+        [HttpPut("id")]
+        public TModel Update(int id,TUpdate request)
+        {
+           return _service.Update(id, request);
+        }
+    }
+}
---
+++ JobSearch.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobSearch.WebAPI.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSearch.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<TModel,Tsearch> : Controller
+    {
+        protected readonly IService<TModel, Tsearch> _service;
+        public BaseController(IService<TModel,Tsearch> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<TModel> Get([FromQuery]Tsearch search)
+        {
+            return _service.Get(search);
+        }
+
+        [HttpGet("id")]
+        public TModel GetById(int id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpDelete("id")]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+    }
+}
